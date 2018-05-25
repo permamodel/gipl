0863dc78469341cda4f5b13cb6b43dad46ce8bdf
Scott <scotts@nsidc.org>
2018-05-25 11:56:40 -0600

    moved functionality of run_gipl() in fortran to the main program

--- gipl/gipl_main.f90
+++ gipl/gipl_main.f90
@@ -14,7 +14,11 @@ program gipl2
 
   implicit none
 
-  character(64) :: empty_filename = ''
+  ! Note: this code could be rewritten to accept a filename from the cmd line
+  character(64) :: filename_passed = ''
+
+  real*8 :: time_reference_counter
+  character(64) :: passed_config_filename
 
   if (iargc() .eq. 1) then
     call getarg(1, fconfig)
@@ -23,7 +27,50 @@ program gipl2
     fconfig = '../examples/gipl_config_3yr.cfg'
   endif
 
+  if (filename_passed .ne. '') then
+    fconfig = filename_passed
+  endif
+
+  ! if configuration file is defined elsewhere, use that
+  ! otherwise, set a default here
+  if (fconfig .eq. '') then
+    fconfig='gipl_config_3yr.cfg'
+    print *, 'No config file specified.  Using: ', fconfig
+  ! Full 135 year config file:
+  !   fconfig='gipl_config.cfg'
+  endif
+
+  passed_config_filename = fconfig
+
   print*,'Running from Fortran with config file: ', fconfig
 
-  call run_gipl(empty_filename)
-end
+  call initialize(passed_config_filename)
+
+  ! Because we want to test both update() and update_until(), and because
+  ! there are write()s both the time of a year's timestep and the timestep
+  ! prior, we need two calls to write_output() at different points in
+  ! the annual cycle
+  do while (time_loop .lt. time_e)
+    print*, 'run_gipl time_loop: ', time_loop
+    time_reference_counter = time_loop
+
+    ! Note: if an adjustment to surface temperature, snow depth, or stcon
+    !    is to be made, it should be made before a call to update_model()
+    !    and to the internal-interpolated grids:
+    !       utemp_i(i_time(i_site)))
+    !       snd_i(i_time(i_site)))
+    !       stcon_i(i_time(i_site)))
+    !    These values were interpolated for the following year at the end
+    !      of the previous year in update_model() (or initialize() for 1st yr
+    call update_model()
+    call update_model_until(time_reference_counter + (n_time - 3) * time_step)
+    call update_model()
+    call update_model()
+    call write_output()
+    call update_model()
+    call write_output()
+  enddo
+
+  call finalize_f90()
+
+end program gipl2
